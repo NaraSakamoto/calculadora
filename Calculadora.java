2889805b5d28ce93fc0171746c4caa7a11e21b9e
alencarp <patriciaalencar775@gmail.com>
2024-05-10 18:47:25 -0300

    feat: Adicionando a subtracao

--- Calculadora.java
+++ Calculadora.java
@@ -2,13 +2,22 @@ import java.util.Scanner;
 
 public class Calculadora {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String linha = scan.nextLine();
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String linha = scan.nextLine();
 
-        String[] valores = linha.split(" ");
-        Soma soma = new Soma();
-        int resultado = soma.somar(Integer.parseInt(valores[0]), Integer.parseInt(valores[2]));
-        System.out.println(resultado);
-    }
+		String[] valores = linha.split(" ");
+
+		if (valores[1].contains("+")) {
+			Soma soma = new Soma();
+			int resultado = soma.somar(Integer.parseInt(valores[0]), Integer.parseInt(valores[2]));
+			System.out.println(resultado);
+		}
+
+		if (valores[1].contains("-")) {
+			Subtracao subtracao = new Subtracao();
+			int resultadoSubtracao = subtracao.subtrair(Integer.parseInt(valores[0]), Integer.parseInt(valores[2]));
+			System.out.println(resultadoSubtracao);
+		}
+	}
 }
